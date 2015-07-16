79bd99871e4843427fce8a2d7d6328a546b7f0af
cristi <cristi.catea@gmail.com>
2015-07-16 15:48:05 -0700

    updates

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
+var mainWindow = null;
+
 app.on('ready', function(){
     var mainWindow = new BrowserWindow({
-        width: '800',
-        heigth: '600'
+        width: '1000',
+        heigth: '700'
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+    // Open the devtools.
+    mainWindow.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
     });
 
 });
---
+++ main.js
@@ -0,0 +1,7 @@
+//var npmList = npmList();
+
+require('./npmMain.js');
+
+var global = new Global();
+
+console.log(global.getList);
