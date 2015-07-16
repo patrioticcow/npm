c0395cc7399ae81cec6a480ad24c447ea854c16a
cristi <cristi.catea@gmail.com>
2015-07-16 16:24:37 -0700

    updates

--- npmMain.js
+++ npmMain.js
@@ -9,12 +9,12 @@ var Global = (function () {
         return exec('npm list --depth=0 -g -json -long', {
             maxBuffer: 500 * 1024
         }, function (error, stdout, stderr) {
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                } else {
-                    return stdout;
-                }
-            });
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            } else {
+                return stdout;
+            }
+        });
     };
 
     return Global;
